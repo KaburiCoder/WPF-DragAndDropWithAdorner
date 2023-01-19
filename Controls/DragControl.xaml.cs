b5a55f0c1f751015570050a8ee9d21bd2e10cc4b
KaburiCorder <specialemail90@gmail.com>
2023-01-20 00:29:04 +0900

    Adorner를 이용한 드래그드롭

---
+++ Controls/DragControl.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfDragAndDrop.Libs;
+
+namespace WpfDragAndDrop.Controls
+{
+  /// <summary>
+  /// DragControl.xaml에 대한 상호 작용 논리
+  /// </summary>
+  public partial class DragControl : UserControl
+  {
+    private ElementAdorner _elementAdorner = default!;
+
+    public DragControl()
+    {
+      InitializeComponent();
+      MouseMove += DragControl_MouseMove;
+      PreviewGiveFeedback += DragControl_PreviewGiveFeedback;
+    }
+
+    private void DragControl_PreviewGiveFeedback(object sender, GiveFeedbackEventArgs e)
+    {
+      _elementAdorner.SetArrange(this);
+    }
+
+    private void DragControl_MouseMove(object sender, MouseEventArgs e)
+    {
+      AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(this);
+      _elementAdorner = new ElementAdorner(this);
+      adornerLayer.Add(_elementAdorner);
+
+      DragDrop.DoDragDrop(this, Background, DragDropEffects.Copy);
+
+      adornerLayer.Remove(_elementAdorner);
+    }
+  }
+}
---
+++ Libs/ElementAdorner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+using WpfLib.Extensions;
+
+namespace WpfDragAndDrop.Libs
+{
+  public class ElementAdorner : Adorner
+  {
+    private Rect _renderRect;
+    private Point _offset;
+    private ImageSource _imageSource;
+
+    public ElementAdorner(FrameworkElement adornedElement) : base(adornedElement)
+    {
+      IsHitTestVisible = false;
+      Opacity = 0.7;
+      
+      _renderRect = new Rect(adornedElement.RenderSize);
+      _offset = new Point(-this._renderRect.Width / 2, -this._renderRect.Height / 2);
+      // element 이미지로 변경
+      using System.Drawing.Bitmap bmp = adornedElement.ToBitmap();
+      _imageSource = bmp.ToImageSource();
+    }
+
+    public void SetArrange(Visual visual)
+    {
+      System.Drawing.Point cursorPoint = System.Windows.Forms.Cursor.Position;
+      Point point = visual.PointFromScreen(new Point(cursorPoint.X + _offset.X, cursorPoint.Y + _offset.Y));
+      Arrange(new Rect(point, DesiredSize));
+    }
+
+    protected override void OnRender(DrawingContext drawingContext)
+    {
+      drawingContext.DrawImage(_imageSource, _renderRect);
+    }
+  }
+}
